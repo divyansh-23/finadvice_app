7030b914148507b1ab004a8e82990be18d440b6f
divyansh-23 <divyansh.s230@gmail.com>
2022-12-05 12:23:24 -0500

    Session all domains

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,13 +2,12 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     #change this as per the react app's local server port
     origins "http://localhost:3001"
-    # origins "*"
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 
   allow do
     #change this as per the heroku app domain
-    origins "https://radiant-stream-07739.herokuapp.com/"
+    origins "https://silly-frangipane-f9d81d.netlify.app"
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 end
