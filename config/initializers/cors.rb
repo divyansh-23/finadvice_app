3095547b456c44b95267864b2b525e42a92e5d8d
divyansh-23 <divyansh.s230@gmail.com>
2022-12-05 12:03:23 -0500

    changed domain urls for prod

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
   allow do
     #change this as per the heroku app domain
-    origins "https://finadvice.herokupp.com"
+    origins "https://radiant-stream-07739.herokuapp.com/"
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 #correct_domain_when_domain_name_exist
 if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, key: "_finadvice_app", domain: "jdh-finadvice-app-api.herukuapp.com"
+  Rails.application.config.session_store :cookie_store, key: "_finadvice_app", domain: "https://radiant-stream-07739.herokuapp.com/"
 else
   Rails.application.config.session_store :cookie_store, key: "_finadvice_app"
 end
